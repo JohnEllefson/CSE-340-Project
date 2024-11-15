25269b786c419c15c43974c317f32714172dbb6d
John Ellefson <johnellefson@byui.edu>
2024-11-15 16:13:33 -0600

    Completed the team activity and assignment 2, including creating the SQL file that can rebuild the Database. Additionally, I fixed some CSS errors and the contrast issue with the floating text.

---
+++ database/assignment2.sql
@@ -0,0 +1,55 @@
+-- Step 1: Add Tony Stark to the account table
+INSERT INTO account (
+        account_firstname,
+        account_lastname,
+        account_email,
+        account_password
+    )
+VALUES (
+        'Tony',
+        'Stark',
+        'tony@starkent.com',
+        'Iam1ronM@n'
+    );
+
+-- Step 2: Change the account type for Tony Stark to 'Admin'
+UPDATE account
+SET account_type = 'Admin'
+WHERE account_firstname = 'Tony'
+    AND account_lastname = 'Stark';
+
+-- Step 3: Delete the Tony Stark account record
+DELETE FROM account
+WHERE account_firstname = 'Tony'
+    AND account_lastname = 'Stark';
+
+-- Step 4: Modify the GM Hummer record.
+UPDATE inventory
+SET inv_description = REPLACE(
+        inv_description,
+        'small interiors',
+        'a huge interior'
+    )
+WHERE inv_make = 'GM'
+    AND inv_model = 'Hummer';
+
+-- Step 5: Use an inner join to select sport vehicles from the inventory table.
+SELECT inv_make,
+    inv_model,
+    classification_name AS classification
+FROM inventory
+    INNER JOIN classification ON inventory.classification_id = classification.classification_id
+WHERE classification_name = 'Sport';
+
+-- Step 6: Update all records in the inventory table to update the file path.
+UPDATE inventory
+SET inv_image = REPLACE(
+        inv_image,
+        '/images/',
+        '/images/vehicles/'
+    ),
+    inv_thumbnail = REPLACE(
+        inv_thumbnail,
+        '/images/',
+        '/images/vehicles/'
+    );
